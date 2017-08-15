3f7afe398c40e89599644c8cf781166cde56ae6a
rajan2 <rajan2@users.noreply.github.com>
2017-08-15 10:10:28 +0200

    Update tkinter_digipot.py
    show warning if wrong input

--- tkinter_digipot.py
+++ tkinter_digipot.py
@@ -195,6 +195,11 @@ class MyPoti(Tkinter.Tk):
 		#Read input value given for pot selection text field
         pot = self.entry_pot.get()
         #if pot < 0 or pot > 4 :
+        if int(pot) < 0 or int(pot) > 4 :
+	        tkMessageBox.showerror("wrong input","wrong input, pot selection must be 0-4, current selection: %s" % pot)
+            return 
+        res = self.entry_res.get()
+
 		
 		#Read input value given for  resistence text field
         res = self.entry_res.get()
